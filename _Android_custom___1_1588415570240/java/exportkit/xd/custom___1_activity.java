e57cb82b682d14d230a7ccbaba4471a25c02c919
Spectrics Solutions <spectricssolutions@gmail.com>
2020-05-02 16:16:09 +0530

    Code Upload

---
+++ _Android_custom___1_1588415570240/java/exportkit/xd/custom___1_activity.java
@@ -0,0 +1,352 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		custom___1
+	 *	@date 		0
+	 *	@title 		custom  1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.2.8.xd
+	 *
+	 */
+	
+
+package exportkit.xd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class custom___1_activity extends Activity {
+
+	
+	private View _bg__custom___1;
+	private View image_6;
+	private ImageView group_63;
+	private ImageView group_99;
+	private View rectangle_29;
+	private ImageView path_2;
+	private View ellipse_1;
+	private ImageView path_3;
+	private ImageView path_4;
+	private ImageView path_5;
+	private View rectangle_4;
+	private View rectangle_6;
+	private ImageView path_6;
+	private TextView type_something;
+	private TextView symptoms;
+	private TextView more;
+	private ImageView path_7;
+	private TextView more_ek1;
+	private ImageView path_7_ek1;
+	private ImageView path_36;
+	private ImageView path_23;
+	private ImageView path_24;
+	private ImageView path_25;
+	private ImageView path_26;
+	private ImageView path_27;
+	private ImageView path_28;
+	private View rectangle_11;
+	private TextView high_fever;
+	private TextView _87_discussion;
+	private ImageView path_22;
+	private ImageView path_29;
+	private ImageView path_30;
+	private ImageView path_31;
+	private ImageView path_32;
+	private ImageView path_33;
+	private ImageView path_34;
+	private ImageView path_35;
+	private View rectangle_12;
+	private TextView tiredness;
+	private TextView _87_discussion_ek1;
+	private ImageView path_21;
+	private View rectangle_10;
+	private TextView dry_cough;
+	private TextView _87_discussion_ek2;
+	private ImageView path_8;
+	private ImageView path_9;
+	private ImageView path_10;
+	private ImageView path_11;
+	private ImageView path_12;
+	private ImageView path_13;
+	private ImageView path_14;
+	private ImageView path_15;
+	private ImageView path_16;
+	private ImageView path_17;
+	private ImageView path_18;
+	private ImageView path_19;
+	private ImageView path_20;
+	private TextView requirements;
+	private TextView covid___19;
+	private View rectangle_14;
+	private View rectangle_20;
+	private View rectangle_21;
+	private TextView sanitizer;
+	private TextView mask;
+	private TextView alcohol;
+	private View rectangle_15;
+	private View rectangle_16;
+	private View rectangle_17;
+	private ImageView path_37;
+	private View rectangle_18;
+	private View rectangle_19;
+	private View ellipse_2;
+	private View ellipse_3;
+	private View ellipse_4;
+	private ImageView path_38;
+	private ImageView path_39;
+	private ImageView path_40;
+	private ImageView path_41;
+	private View rectangle_22;
+	private View rectangle_23;
+	private View rectangle_24;
+	private ImageView path_42;
+	private View rectangle_25;
+	private View rectangle_26;
+	private ImageView path_104;
+	private View rectangle_27;
+	private ImageView path_43;
+	private ImageView path_44;
+	private ImageView path_45;
+	private ImageView path_46;
+	private ImageView path_47;
+	private ImageView path_48;
+	private ImageView path_49;
+	private ImageView path_50;
+	private ImageView path_51;
+	private ImageView path_52;
+	private ImageView path_53;
+	private ImageView path_54;
+	private ImageView path_55;
+	private ImageView path_56;
+	private ImageView path_57;
+	private ImageView path_58;
+	private ImageView path_59;
+	private ImageView path_60;
+	private ImageView path_61;
+	private ImageView path_62;
+	private ImageView path_63;
+	private ImageView path_64;
+	private ImageView path_65;
+	private ImageView path_66;
+	private ImageView path_67;
+	private ImageView path_68;
+	private ImageView path_69;
+	private ImageView path_70;
+	private ImageView path_71;
+	private ImageView path_72;
+	private ImageView path_73;
+	private ImageView path_74;
+	private View ellipse_5;
+	private View ellipse_6;
+	private ImageView path_75;
+	private ImageView path_76;
+	private ImageView path_77;
+	private ImageView path_78;
+	private ImageView path_79;
+	private ImageView path_80;
+	private ImageView path_81;
+	private ImageView path_82;
+	private View ellipse_7;
+	private View ellipse_8;
+	private View ellipse_9;
+	private ImageView path_83;
+	private ImageView path_84;
+	private ImageView path_85;
+	private ImageView path_86;
+	private ImageView path_87;
+	private ImageView path_88;
+	private ImageView path_89;
+	private ImageView path_90;
+	private ImageView path_91;
+	private ImageView path_92;
+	private ImageView path_93;
+	private ImageView path_94;
+	private ImageView path_95;
+	private ImageView path_96;
+	private ImageView path_97;
+	private ImageView path_98;
+	private ImageView path_99;
+	private ImageView path_100;
+	private ImageView path_101;
+	private View rectangle_28;
+	private TextView learn_more;
+	private ImageView path_103;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.custom___1);
+
+		
+		_bg__custom___1 = (View) findViewById(R.id._bg__custom___1);
+		image_6 = (View) findViewById(R.id.image_6);
+		group_63 = (ImageView) findViewById(R.id.group_63);
+		group_99 = (ImageView) findViewById(R.id.group_99);
+		rectangle_29 = (View) findViewById(R.id.rectangle_29);
+		path_2 = (ImageView) findViewById(R.id.path_2);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		path_3 = (ImageView) findViewById(R.id.path_3);
+		path_4 = (ImageView) findViewById(R.id.path_4);
+		path_5 = (ImageView) findViewById(R.id.path_5);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		rectangle_6 = (View) findViewById(R.id.rectangle_6);
+		path_6 = (ImageView) findViewById(R.id.path_6);
+		type_something = (TextView) findViewById(R.id.type_something);
+		symptoms = (TextView) findViewById(R.id.symptoms);
+		more = (TextView) findViewById(R.id.more);
+		path_7 = (ImageView) findViewById(R.id.path_7);
+		more_ek1 = (TextView) findViewById(R.id.more_ek1);
+		path_7_ek1 = (ImageView) findViewById(R.id.path_7_ek1);
+		path_36 = (ImageView) findViewById(R.id.path_36);
+		path_23 = (ImageView) findViewById(R.id.path_23);
+		path_24 = (ImageView) findViewById(R.id.path_24);
+		path_25 = (ImageView) findViewById(R.id.path_25);
+		path_26 = (ImageView) findViewById(R.id.path_26);
+		path_27 = (ImageView) findViewById(R.id.path_27);
+		path_28 = (ImageView) findViewById(R.id.path_28);
+		rectangle_11 = (View) findViewById(R.id.rectangle_11);
+		high_fever = (TextView) findViewById(R.id.high_fever);
+		_87_discussion = (TextView) findViewById(R.id._87_discussion);
+		path_22 = (ImageView) findViewById(R.id.path_22);
+		path_29 = (ImageView) findViewById(R.id.path_29);
+		path_30 = (ImageView) findViewById(R.id.path_30);
+		path_31 = (ImageView) findViewById(R.id.path_31);
+		path_32 = (ImageView) findViewById(R.id.path_32);
+		path_33 = (ImageView) findViewById(R.id.path_33);
+		path_34 = (ImageView) findViewById(R.id.path_34);
+		path_35 = (ImageView) findViewById(R.id.path_35);
+		rectangle_12 = (View) findViewById(R.id.rectangle_12);
+		tiredness = (TextView) findViewById(R.id.tiredness);
+		_87_discussion_ek1 = (TextView) findViewById(R.id._87_discussion_ek1);
+		path_21 = (ImageView) findViewById(R.id.path_21);
+		rectangle_10 = (View) findViewById(R.id.rectangle_10);
+		dry_cough = (TextView) findViewById(R.id.dry_cough);
+		_87_discussion_ek2 = (TextView) findViewById(R.id._87_discussion_ek2);
+		path_8 = (ImageView) findViewById(R.id.path_8);
+		path_9 = (ImageView) findViewById(R.id.path_9);
+		path_10 = (ImageView) findViewById(R.id.path_10);
+		path_11 = (ImageView) findViewById(R.id.path_11);
+		path_12 = (ImageView) findViewById(R.id.path_12);
+		path_13 = (ImageView) findViewById(R.id.path_13);
+		path_14 = (ImageView) findViewById(R.id.path_14);
+		path_15 = (ImageView) findViewById(R.id.path_15);
+		path_16 = (ImageView) findViewById(R.id.path_16);
+		path_17 = (ImageView) findViewById(R.id.path_17);
+		path_18 = (ImageView) findViewById(R.id.path_18);
+		path_19 = (ImageView) findViewById(R.id.path_19);
+		path_20 = (ImageView) findViewById(R.id.path_20);
+		requirements = (TextView) findViewById(R.id.requirements);
+		covid___19 = (TextView) findViewById(R.id.covid___19);
+		rectangle_14 = (View) findViewById(R.id.rectangle_14);
+		rectangle_20 = (View) findViewById(R.id.rectangle_20);
+		rectangle_21 = (View) findViewById(R.id.rectangle_21);
+		sanitizer = (TextView) findViewById(R.id.sanitizer);
+		mask = (TextView) findViewById(R.id.mask);
+		alcohol = (TextView) findViewById(R.id.alcohol);
+		rectangle_15 = (View) findViewById(R.id.rectangle_15);
+		rectangle_16 = (View) findViewById(R.id.rectangle_16);
+		rectangle_17 = (View) findViewById(R.id.rectangle_17);
+		path_37 = (ImageView) findViewById(R.id.path_37);
+		rectangle_18 = (View) findViewById(R.id.rectangle_18);
+		rectangle_19 = (View) findViewById(R.id.rectangle_19);
+		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+		ellipse_3 = (View) findViewById(R.id.ellipse_3);
+		ellipse_4 = (View) findViewById(R.id.ellipse_4);
+		path_38 = (ImageView) findViewById(R.id.path_38);
+		path_39 = (ImageView) findViewById(R.id.path_39);
+		path_40 = (ImageView) findViewById(R.id.path_40);
+		path_41 = (ImageView) findViewById(R.id.path_41);
+		rectangle_22 = (View) findViewById(R.id.rectangle_22);
+		rectangle_23 = (View) findViewById(R.id.rectangle_23);
+		rectangle_24 = (View) findViewById(R.id.rectangle_24);
+		path_42 = (ImageView) findViewById(R.id.path_42);
+		rectangle_25 = (View) findViewById(R.id.rectangle_25);
+		rectangle_26 = (View) findViewById(R.id.rectangle_26);
+		path_104 = (ImageView) findViewById(R.id.path_104);
+		rectangle_27 = (View) findViewById(R.id.rectangle_27);
+		path_43 = (ImageView) findViewById(R.id.path_43);
+		path_44 = (ImageView) findViewById(R.id.path_44);
+		path_45 = (ImageView) findViewById(R.id.path_45);
+		path_46 = (ImageView) findViewById(R.id.path_46);
+		path_47 = (ImageView) findViewById(R.id.path_47);
+		path_48 = (ImageView) findViewById(R.id.path_48);
+		path_49 = (ImageView) findViewById(R.id.path_49);
+		path_50 = (ImageView) findViewById(R.id.path_50);
+		path_51 = (ImageView) findViewById(R.id.path_51);
+		path_52 = (ImageView) findViewById(R.id.path_52);
+		path_53 = (ImageView) findViewById(R.id.path_53);
+		path_54 = (ImageView) findViewById(R.id.path_54);
+		path_55 = (ImageView) findViewById(R.id.path_55);
+		path_56 = (ImageView) findViewById(R.id.path_56);
+		path_57 = (ImageView) findViewById(R.id.path_57);
+		path_58 = (ImageView) findViewById(R.id.path_58);
+		path_59 = (ImageView) findViewById(R.id.path_59);
+		path_60 = (ImageView) findViewById(R.id.path_60);
+		path_61 = (ImageView) findViewById(R.id.path_61);
+		path_62 = (ImageView) findViewById(R.id.path_62);
+		path_63 = (ImageView) findViewById(R.id.path_63);
+		path_64 = (ImageView) findViewById(R.id.path_64);
+		path_65 = (ImageView) findViewById(R.id.path_65);
+		path_66 = (ImageView) findViewById(R.id.path_66);
+		path_67 = (ImageView) findViewById(R.id.path_67);
+		path_68 = (ImageView) findViewById(R.id.path_68);
+		path_69 = (ImageView) findViewById(R.id.path_69);
+		path_70 = (ImageView) findViewById(R.id.path_70);
+		path_71 = (ImageView) findViewById(R.id.path_71);
+		path_72 = (ImageView) findViewById(R.id.path_72);
+		path_73 = (ImageView) findViewById(R.id.path_73);
+		path_74 = (ImageView) findViewById(R.id.path_74);
+		ellipse_5 = (View) findViewById(R.id.ellipse_5);
+		ellipse_6 = (View) findViewById(R.id.ellipse_6);
+		path_75 = (ImageView) findViewById(R.id.path_75);
+		path_76 = (ImageView) findViewById(R.id.path_76);
+		path_77 = (ImageView) findViewById(R.id.path_77);
+		path_78 = (ImageView) findViewById(R.id.path_78);
+		path_79 = (ImageView) findViewById(R.id.path_79);
+		path_80 = (ImageView) findViewById(R.id.path_80);
+		path_81 = (ImageView) findViewById(R.id.path_81);
+		path_82 = (ImageView) findViewById(R.id.path_82);
+		ellipse_7 = (View) findViewById(R.id.ellipse_7);
+		ellipse_8 = (View) findViewById(R.id.ellipse_8);
+		ellipse_9 = (View) findViewById(R.id.ellipse_9);
+		path_83 = (ImageView) findViewById(R.id.path_83);
+		path_84 = (ImageView) findViewById(R.id.path_84);
+		path_85 = (ImageView) findViewById(R.id.path_85);
+		path_86 = (ImageView) findViewById(R.id.path_86);
+		path_87 = (ImageView) findViewById(R.id.path_87);
+		path_88 = (ImageView) findViewById(R.id.path_88);
+		path_89 = (ImageView) findViewById(R.id.path_89);
+		path_90 = (ImageView) findViewById(R.id.path_90);
+		path_91 = (ImageView) findViewById(R.id.path_91);
+		path_92 = (ImageView) findViewById(R.id.path_92);
+		path_93 = (ImageView) findViewById(R.id.path_93);
+		path_94 = (ImageView) findViewById(R.id.path_94);
+		path_95 = (ImageView) findViewById(R.id.path_95);
+		path_96 = (ImageView) findViewById(R.id.path_96);
+		path_97 = (ImageView) findViewById(R.id.path_97);
+		path_98 = (ImageView) findViewById(R.id.path_98);
+		path_99 = (ImageView) findViewById(R.id.path_99);
+		path_100 = (ImageView) findViewById(R.id.path_100);
+		path_101 = (ImageView) findViewById(R.id.path_101);
+		rectangle_28 = (View) findViewById(R.id.rectangle_28);
+		learn_more = (TextView) findViewById(R.id.learn_more);
+		path_103 = (ImageView) findViewById(R.id.path_103);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
